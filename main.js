37c73f6ab2dced0c1d7469df0c894a5cb9dd4a28
pashokman <pashokman1@gmail.com>
2022-07-29 21:37:10 +0300

    solution
    v2 optimisation

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 const invertCase = (str) => {
     let result = '';
 
-    if (str === '') {
-        return `It's empty string!`
-    } else {
-        for (let index = 0; index < str.length; index++) {
-            if (str[index] === str[index].toUpperCase()) {
-                result += `${str[index].toLowerCase()}`;
-            } else if (str[index] === str[index].toLowerCase()) {
-                result += `${str[index].toUpperCase()}`;
-            } else {
-                result += `${str[index]}`;
-            }
-        }
+    if (str === '') return `It's empty string!`;
 
-        return result;
+    for (let index = 0; index < str.length; index++) {
+        if (str[index] === str[index].toUpperCase()) {
+            result += `${str[index].toLowerCase()}`;
+        } else if (str[index] === str[index].toLowerCase()) {
+            result += `${str[index].toUpperCase()}`;
+        } else {
+            result += `${str[index]}`;
+        }
     }
+
+    return result;
+
 }
 
-console.log(invertCase('Hello, World!')); // hELLO, wORLD!
-console.log(invertCase('I loVe JS'));     // i LOvE js
+console.log(invertCase(''));                // It's empty string!
+console.log(invertCase('Hello, World!'));   // hELLO, wORLD!
+console.log(invertCase('I loVe JS'));       // i LOvE js
